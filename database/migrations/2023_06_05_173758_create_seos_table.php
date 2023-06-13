4c47aabf3e6f42931eb9b2a2df8d975464fbeba3
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2023-06-13 11:10:28 +0100

    add same infos to the seos table

--- database/migrations/2023_06_05_173758_create_seos_table.php
+++ database/migrations/2023_06_05_173758_create_seos_table.php
@@ -16,9 +16,12 @@ return new class extends Migration
             $table->uuid();
 
             $table->morphs('seoable');
+            
             $table->string('meta_title')->nullable();
             $table->string('meta_url')->nullable();
             $table->longText('meta_description')->nullable();
+            $table->json('meta_keywords')->nullable();
+            $table->json('meta_robos')->nullable();
 
             /***og facebook ***/
             //https://www.ionos.fr/digitalguide/web-marketing/les-media-sociaux/open-graph/
@@ -33,8 +36,11 @@ return new class extends Migration
             $table->json('og_property')->nullable(); // like article:published_time,article:section;...
 
             /***og twitter ***/
+            $table->string('twitter_card')->nullable();
+            $table->string('twitter_site')->nullable();
             $table->string('twitter_title')->nullable();
             $table->longText('twitter_description')->nullable();
+            $table->string('twitter_image')->nullable();
 
             $table->boolean('is_default')->default(false);
             $table->timestamps();
