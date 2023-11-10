872822fa5cd679683e5098d3fb0c4f91088f48fb
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-11-10 17:35:25 +0100

    add spatie/cacheresponse

--- app/Traits/CacheCleaner.php
+++ app/Traits/CacheCleaner.php
@@ -3,21 +3,24 @@
 namespace App\Traits;
 
 use Illuminate\Support\Facades\Artisan;
-
+use Spatie\ResponseCache\Facades\ResponseCache;
 trait CacheCleaner
 {
     public static function bootCacheCleaner()
     {
         self::created(function () {
             Artisan::call('cache:clear');
+            ResponseCache::clear();
         });
 
         self::updated(function () {
             Artisan::call('cache:clear');
+            ResponseCache::clear();
         });
 
         self::deleted(function () {
             Artisan::call('cache:clear');
+            ResponseCache::clear();
         });
     }
 }
